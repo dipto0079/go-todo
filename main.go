afd124474612c243a5b23038cc9aea39e54f0221
dipto0079 <sudiptobiswas.coder@gmail.com>
2021-11-10 23:55:55 +0600

    class 4 Todo

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	http.HandleFunc("/", h.Home)
 	http.HandleFunc("/todos/create", h.CreateTodo)
 	log.Println("Server starting .............")
-	//if err := http.ListenAndServe("127.0.0.1:3000", nil); err != nil {
-	//	log.Fatal(err)
-	//}
+	if err := http.ListenAndServe("127.0.0.1:3000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
